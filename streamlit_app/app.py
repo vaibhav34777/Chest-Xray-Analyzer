689c6c92d33c831d13340547f5b2420e7a5b6d91
vaibhav34777 <162586561+vaibhav34777@users.noreply.github.com>
2025-04-10 13:37:13 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -16,7 +16,7 @@ def download_model():
     model_path = "model/model.pth"
     if not os.path.exists(model_path):
         os.makedirs("model", exist_ok=True)
-        url = "https://huggingface.co/imvaibhavrana/chest-xray-analyzer/resolve/main/model.pt?download=true
+        url = "https://huggingface.co/imvaibhavrana/chest-xray-analyzer/resolve/main/model.pt?download=true"
 "
         response = requests.get(url)
         with open(model_path, "wb") as f:
