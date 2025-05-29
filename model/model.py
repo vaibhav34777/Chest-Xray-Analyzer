c8b3e682b0e0c9798e976abbd7dcd63d4ccf8600
Vaibhav Rana <162586561+vaibhav34777@users.noreply.github.com>
2025-05-29 15:42:06 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 # MODEL ARCHITECTURE
+from torch import nn
  # 3x3 convolution with padding
 def conv3x3(in_planes,out_planes,stride=1,dilation=1):  
     return nn.Conv2d(in_planes,out_planes,kernel_size=3,
